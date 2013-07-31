81d54923dc557f1cd9a0c0212899d7a7f6cfc4c7
Aidan Feldman <aidan.feldman@gmail.com>
2013-07-31 02:30:25 -0400

    add dummy rspec test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,23 @@
 # specs live under a `spec` directory, which RSpec adds to the `$LOAD_PATH`.
 # Require this file using `require "spec_helper"` to ensure that it is only
 # loaded once.
-#
+
+require 'rubygems'
+require 'bundler/setup'
+Bundler.setup
+
+require 'action_mailer'
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+
+  ActionMailer::Base.delivery_method = :test
+  config.before do
+    ActionMailer::Base.deliveries.clear
+  end
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
