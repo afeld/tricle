af2f9450912afde27596ded3da2b3067669eadeb
Aidan Feldman <aidan.feldman@gmail.com>
2013-08-01 20:47:25 -0400

    email: add comparison to previous week

--- lib/tricle/helpers/email_helper.rb
+++ lib/tricle/helpers/email_helper.rb
@@ -5,8 +5,16 @@ module Tricle
         Date.today.beginning_of_week
       end
 
+      def weeks_ago(n)
+        self.beginning_of_week.weeks_ago(n)
+      end
+
       def format_date(date)
         date.strftime('%-m/%-d/%y')
+      end
+
+      def fraction(numerator, denominator)
+        sprintf('%.1f%', (numerator / denominator.to_f) * 100.0)
       end
     end
   end
