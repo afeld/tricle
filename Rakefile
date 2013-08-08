41b7b37f901f448b79e6eb8c0169108a66f82349
Aidan Feldman <aidan.feldman@gmail.com>
2013-08-08 03:00:30 +0200

    attempt to fix require problem for ruby 1.9.3 on travis
    https://travis-ci.org/artsy/tricle/jobs/9967512

--- Rakefile
+++ Rakefile
@@ -1,6 +1,6 @@
 require 'bundler/gem_tasks'
 require 'rspec/core/rake_task'
-require 'tricle/tasks'
+require_relative 'lib/tricle/tasks'
 
 RSpec::Core::RakeTask.new(:spec)
 
--- lib/tricle.rb
+++ lib/tricle.rb
@@ -3,10 +3,13 @@ require 'active_support/core_ext/date/calculations'
 require 'active_support/core_ext/numeric/time'
 require 'active_support/descendants_tracker'
 
+EXCLUDED_FILES = %w(mail_preview.rb tasks.rb).to_set.freeze
+
 dir = File.dirname(__FILE__)
 Dir[File.join(dir, 'tricle', '**', '*.rb')].each do |file|
   # MailPreview should be require'd explcitly
-  require file unless file.end_with?('mail_preview.rb')
+  basename = File.basename(file)
+  require file unless EXCLUDED_FILES.include?(basename)
 end
 
 ActionMailer::Base.view_paths = File.join(dir, 'tricle')
