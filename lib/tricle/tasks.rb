bef6f35c0cdfb4169c1e686fca5226038d9d5751
Aidan Feldman <aidan.feldman@gmail.com>
2013-08-11 23:09:01 +0100

    move the test app require-ing into the Rakefile

--- lib/tricle/tasks.rb
+++ lib/tricle/tasks.rb
@@ -5,9 +5,6 @@ require_relative '../tricle'
 namespace :tricle do
   desc "Start a local server to preview your mailers"
   task :preview do
-    require_relative '../../spec/app/test_metric.rb'
-    require_relative '../../spec/app/test_report.rb'
-    require_relative '../../spec/app/test_mailer.rb'
     require_relative 'mail_preview'
 
     Rack::Server.start app: Tricle::MailPreview
