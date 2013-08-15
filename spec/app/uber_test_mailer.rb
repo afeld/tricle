606d0bd0e0fb2292aafe7132fd939d717da7605a
Aidan Feldman <aidan.feldman@gmail.com>
2013-08-15 17:32:59 -0400

    fix UberTestMailer

--- spec/app/uber_test_mailer.rb
+++ spec/app/uber_test_mailer.rb
@@ -1,8 +1,8 @@
-require_relative '../../lib/tricle/mailer'
+require_relative '../../lib/tricle/mailer/weekly'
 require_relative 'test_metric'
 require_relative 'test_metric_with_long_name'
 
-class UberTestMailer < Tricle::Mailer
+class UberTestMailer < Tricle::Mailer::Weekly
   default(to: 'recipient1@test.com', from: 'sender@test.com')
 
   metric TestMetric
