f9ea032efc6a97319292f673e89ea8b5830379d7
Aidan Feldman <aidan.feldman@gmail.com>
2013-08-16 12:14:55 -0400

    move all header definitions into presenter

--- lib/tricle/presenters/report/weekly.rb
+++ lib/tricle/presenters/report/weekly.rb
@@ -5,6 +5,19 @@ module Tricle
   module Presenters
     module Report
       class Weekly < Base
+        def headers
+          [
+            self.title_header,
+            self.last_week_header,
+            self.previous_week_header,
+            self.quarterly_average_header
+          ]
+        end
+
+        def title_header
+          ''
+        end
+
         def weeks_ago(n)
           self.now.beginning_of_week.weeks_ago(n)
         end
@@ -13,16 +26,28 @@ module Tricle
           Tricle::Presenters::Helper.dates_cell(start_at, start_at.end_of_week)
         end
 
-        def last_week_dates_cell
+        def last_week_dates_range
           single_week_dates_cell(weeks_ago(1))
         end
 
-        def previous_week_dates_cell
+        def last_week_header
+          "<div>Last week</div>#{self.last_week_dates_range}"
+        end
+
+        def previous_week_dates_range
           single_week_dates_cell(weeks_ago(2))
         end
 
-        def quarter_dates_cell
+        def previous_week_header
+          "<div>Previous week</div>#{self.previous_week_dates_range}"
+        end
+
+        def quarter_dates_range
           Tricle::Presenters::Helper.dates_cell(weeks_ago(13), weeks_ago(1).end_of_week)
+        end
+
+        def quarterly_average_header
+          "<div>Quarterly average</div>#{self.quarter_dates_range}"
         end
 
         def list_markup(list)
