f90e2cbc7bf60486c928ff4482f69fca3a490f13
Aidan Feldman <afeld@github.com>
2014-04-07 18:07:46 -0700

    remove timezone from timecop resetting

--- spec/config/timecop.rb
+++ spec/config/timecop.rb
@@ -2,6 +2,6 @@ require 'timecop'
 
 def reset_time
   # need to freeze the time for the TestMetric
-  now = Time.new(2013, 8, 1, 1, 0, 0, '-04:00')
+  now = Time.new(2013, 8, 1, 1, 0, 0)
   Timecop.freeze(now)
 end
