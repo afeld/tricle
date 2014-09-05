c7cbc4effb13d14900aa50fee9fb5583469172b5
Aidan Feldman <aidan.feldman@gmail.com>
2014-09-05 01:03:48 -0400

    slim rails previewer even further
    based on https://gist.github.com/josevalim/1942658

--- app/config/application.rb
+++ app/config/application.rb
@@ -9,16 +9,7 @@ Bundler.require(*Rails.groups)
 
 module Tester
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.eager_load = false
+    config.secret_key_base = '37ceb075066e48d67b8d409fd43e91dee4be553274d919441ecb72f930196ac5dd255eb336e59f1d18679c06ed4f7681a3a729d01aef70761d90330cfe9c537b'
   end
 end
